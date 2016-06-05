c3c2eaa57acf308b8e81461bb8d426d169f82517
Alex Pizarro <alex.pizarro.j@gmail.com>
2016-06-05 14:28:33 -0400

    Initial commit

---
+++ app/init.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+require_once 'database.php';
+
+require_once 'core/App.php';
+
+require_once 'core/Controller.php';
+
+// Timezone config
+date_default_timezone_set('America/La_Paz');
